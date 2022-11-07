e69ed44cf043424de34a0721edb92ac5b7051beb
Lily Silva <lilys.prog@gmail.com>
2022-11-07 10:34:22 -0300

    aula sobre aplicação de métodos

--- Aula004/src/aula004/Aula004.java
+++ Aula004/src/aula004/Aula004.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+// Aula prática 04 - métodos getter, setter e construct (07/11/2022)
+
 package aula004;
 
-/**
- *
- * @author lyli
- */
-public class Aula004 {
-
-    /**
-     * @param args the command line arguments
-     */
+public class Aula004 {    
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        Caneta c1 = new Caneta("Bic", "Vermelha", 0.7f); //parâmetros para o construtor
+        c1.status();
+        
+        Caneta c2 = new Caneta("MontBlanc", "Preta", 0.5f);
+        c2.status();
+//        System.out.println("Tenho uma caneta " + c1.getModelo() + ".");
+//        System.out.println("A ponta dela é de " + c1.getPonta() + "mm.");
+    }    
 }
--- Aula004/src/aula004/Caneta.java
+++ Aula004/src/aula004/Caneta.java
@@ -2,27 +2,43 @@
 package aula004;
 public class Caneta {
     // definir visibilidade dos atributos e dos métodos
-    public String modelo;
-    public String cor;
+    private String modelo;    
     private float ponta;
-    protected int carga;
+    private String cor;
     private boolean tampada;
     
-    // void significa 'sem retorno'    
-    public void status() {
-        System.out.println("Modelo: " + this.modelo + ".");
-        System.out.println("Uma caneta " + this.cor + ".");
-        System.out.println("Está tampada? " + this.tampada + ".");
-        System.out.println("Qual a ponta? " + this.ponta + ".");
-        System.out.println("Carga: " + this.carga + "%.");
+    // criando um método construtor, usa-se o mesmo nome da classe
+    // desta formma, para encontrar esse método, basta procurar 
+    // um com o mesmo nome da classe principal
+//    public Caneta() {
+//        this.tampar();
+//        this.cor = "Azul";
+//    }
+// criando um construtor que recebe parâmetros:
+    
+    public Caneta(String m, String c, float p) {
+        this.modelo = m;
+        this.cor = c;
+        this.setPonta(p);
+        this.tampar();
     }
     
-    public void rabiscar() {
-       if (this.tampada == true) {
-           System.out.println("ERRO! Não posso rabiscar!");
-       } else {             
-           System.out.println("Estou rabiscando!");
-       }
+    
+    // usa-se o tipo 'String' pq se usar 'void' seria sem retorno   
+    public String getModelo() {
+        return this.modelo;
+    }
+    
+    public void setModelo(String m) {
+        this.modelo = m;
+    }
+    
+    public float getPonta() {
+        return this.ponta;
+    }
+    
+    public void setPonta(float p) {
+        this.ponta = p;
     }
     
     public void tampar() {
@@ -32,5 +48,14 @@ public class Caneta {
     public void destampar() {
         this.tampada = false;
     }
-    
+
+    public void status() {
+        System.out.println("Sobre a caneta: ");
+        System.out.println("Modelo: " + this.modelo);
+        // ao invés de 'this.modelo' pode-se usar 'this.getModelo().
+        System.out.println("Ponta: " + this.ponta);
+        // a mesma coisa para 'this.ponta'
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada: " + this.tampada);        
+    }    
 }
