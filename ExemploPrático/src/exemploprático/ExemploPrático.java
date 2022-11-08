b63f1b068437996f0a2183d951bc24f33ab5c4ca
Lily Silva <lilys.prog@gmail.com>
2022-11-08 12:23:25 -0300

    exemplo prático de classes

---
+++ ExemploPrático/src/exemploprático/ExemploPrático.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package exemploprático;
+
+public class ExemploPrático {
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
