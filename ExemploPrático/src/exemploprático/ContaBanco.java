99d734553257bcf95d28b02e05a79c3f1711a297
Lily Silva <lilys.prog@gmail.com>
2022-11-08 20:25:17 -0300

    finalizei exercício, falta comparar

--- ExemploPrático/src/exemploprático/ContaBanco.java
+++ ExemploPrático/src/exemploprático/ContaBanco.java
@@ -1,34 +1,118 @@
 // neste arquivo cria-se os atributos e métodos da classe
 package exemploprático;
+import java.util.Random;
 
 public class ContaBanco {
     
     public int numConta;
+    public String cc;
+    public String cp;
+    public int num;
     protected String tipo;
     private String dono;
     private float saldo;    
-    private float deposito;
+    private float val;
     private boolean status;
 
+    // método construtor
     public ContaBanco(float saldo, boolean status) {
         this.saldo = 0.00f;
         this.status = false;
-    }
+    }    
     
     public void abrirConta() {
-        status = true;
-    }
+        status = true;        
+    }    
     
     public void fecharConta() {
-        if (saldo == 0) {
+        if (saldo == 0f) {
             status = false;
+        } else {
+            System.out.println("A conta não pode ser fechada pois possui saldo ou dívida em aberto."); 
         }        
     }
     
-    public void depositar() {
+    public void depositar(float val) {
         if (status == true) {
-            saldo += deposito;
-        }
+            saldo += val;            
+        } else {
+            System.out.println("Não existe conta para fazer o depósito");
+        }                 
     }
     
+    public void sacar(float val) {
+        if ((status == true) && (saldo > 0f)) {
+           saldo -= val; 
+        }        
+    }  
+    
+    public void pagarMensal() {
+        if (tipo == "Conta Corrente") {
+            saldo -= 12f;            
+        } else if (tipo == "Conta Poupança"){
+            saldo -= 20f;
+        }        
+    }
+    
+    public void setStatus(boolean status) {
+        this.status = status;                
+    }
+    
+    public boolean getStatus() {
+        return this.status;
+    }
+        
+    public void setNumConta(int num) {        
+        this.numConta = num;
+    }
+        
+    public int getNumConta() {
+        return this.numConta;                
+    }
+    
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+        if (status == true){
+           if (tipo == "cc") {
+                this.tipo = "Conta Corrente";
+                saldo += 50f;
+            } else if (tipo == "cp") {
+                this.tipo = "Conta Poupança";
+                saldo += 150f;
+            } 
+        } else {
+            this.tipo = "Não existe esse tipo de conta no banco.";
+            saldo = 0;
+        }        
+    }
+    
+    public String getTipo() {
+        return this.tipo;
+    }
+    
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+    
+    public String getDono() {
+        return this.dono;
+    }
+    
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
+    public float getSaldo() {
+        return this.saldo;
+    }
+    
+    
+   // método para mostrar os dados da conta
+    public void estado() {
+        System.out.println("A conta está aberta? " + this.getStatus() + ".");
+        System.out.println("O número da conta é " + this.getNumConta() + ".");
+        System.out.println("O tipo de conta é " + this.getTipo() + ".");
+        System.out.println("O dono desta conta é " + this.getDono() + ".");
+        System.out.println("O saldo em conta é de " + this.getSaldo() + " reais.");   
+    }   
 }
--- ExemploPrático/src/exemploprático/ExemploPrático.java
+++ ExemploPrático/src/exemploprático/ExemploPrático.java
@@ -3,8 +3,29 @@ package exemploprático;
 
 public class ExemploPrático {
     
+        
     public static void main(String[] args) {
-        ContaBanco c1 = new ContaBanco();
+        
+        ContaBanco conta1 = new ContaBanco(0.00f, false);
+        
+        conta1.abrirConta();
+        conta1.depositar(300f);        
+        conta1.setTipo("cp");        
+        conta1.pagarMensal(); 
+        conta1.setDono("Jubileu");        
+        conta1.estado();
+        
+        System.out.println("=============================");
+        
+        ContaBanco conta2 = new ContaBanco(0.00f, false);
+        
+        conta2.abrirConta();
+        conta2.depositar(500f);
+        conta2.setTipo("cc");
+        conta2.pagarMensal();
+        conta2.setDono("Creuza");
+        conta2.estado();
+        
     }
 
 }
